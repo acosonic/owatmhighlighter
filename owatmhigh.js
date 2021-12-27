4b46c9ae6021cbdd283d066cefc1646bf8abcd0f
Aleksandar Pavić <acosonic@gmail.com>
2021-12-27 08:16:43 +0100

    Update owatmhigh.js
    Added debug show variable.
    
    Prevented duplicate loads

--- owatmhigh.js
+++ owatmhigh.js
@@ -1,43 +1,46 @@
 // ==UserScript==
 // @name         OutlookPaint
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.1
 // @description  try to take over the world!
-// @author       Aleksandar Pavić https://www.linkedin.com/in/acosonic/detail/recent-activity/posts/
-// @match        https://changethis/owa/
+// @author       Aleksandar Pavić
+// @match        https://mail.ns.rs/owa/
 // @icon         https://www.google.com/s2/favicons?domain=ns.rs
 // @grant        none
 // @require      http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
-var hlCol = "#ffe6e6"; //Change this for highlighted color for unread messages
+var hlCol = "#fffce8"; //Change this for highlighted color for unread messages
 var bgCol = "#ffffff"; //this is read messages
+var debug = 0; //turn on debugging
+window.loaded = 0; //prevent multiple loads and multiple bindings of events
+
 
 $(document).ready(function(){
-
-    setTimeout(function(){ window.OutlookPaint(); }, 1000);
-    //console.log("triggered");
-
-});
-
-window.OutlookPaint = function() {
+if (window.loaded == 0) {
     //performing highlights
+    $("._lvv_T").css("font-size","15px");
+    $("._lvv_T").css("color","black");
+    $("[autoid='_lvv_5']").css("font-size","14px");
     $("._lvv_w").not("._lvv_y").css("background",hlCol,"important");
     $(".scrollContainer").on('scroll',function() {
        $("._lvv_w").not("._lvv_y").css("background",hlCol,"important");
+       $("._lvv_y").css("background",bgCol,"important");
+        if (debug == 1) console.log("highlighting on scroll");
     });
 
     //removing highlight on click
     $(".scrollContainer").on('click',function(){
         $("._lvv_y").css("background",bgCol,"important");
+        if (debug == 1) console.log("removing highlight on click");
     });
 
     //highlighting new messages
     $('.scrollContainer').bind('DOMSubtreeModified', function(){
         $("._lvv_w").not("._lvv_y").css("background",hlCol,"important");
+        if (debug == 1) console.log("owapaint done");
     });
-      //  console.log("finished OutlookPaint");
-
+    if (debug == 1) console.log("owapaint done");
 }
-
-
+    window.loaded = 1;
+});
