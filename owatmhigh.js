4b4a2bfb5cbd4670d4e658f0692133e4a1953ef0
Aleksandar Pavić <acosonic@gmail.com>
2021-12-27 08:17:11 +0100

    Update owatmhigh.js

--- owatmhigh.js
+++ owatmhigh.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       Aleksandar Pavić
-// @match        https://mail.ns.rs/owa/
+// @match        https://youroutlook/owa/
 // @icon         https://www.google.com/s2/favicons?domain=ns.rs
 // @grant        none
 // @require      http://code.jquery.com/jquery-latest.js
